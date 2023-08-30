65702cd00815593654e1eccabf7c32ad06c44696
Anliza <nnanliza.gmail.com>
2023-08-30 19:37:52 +0300

    added detailConfirmation method

--- src/main/java/com/systechafrica/AtmMachine/ATMMachine.java
+++ src/main/java/com/systechafrica/AtmMachine/ATMMachine.java
@@ -10,9 +10,10 @@ public class ATMMachine {
     public static void main(String[] args) {
        ATMMachine app = new ATMMachine();
        
+       app.detailConfirmation();
        app.displayPage();
        }
-       
+
     public void displayPage(){
         System.out.println ("********************");
         System.out.println ("ATM SIMULATOR");
@@ -26,5 +27,40 @@ public class ATMMachine {
         System.out.println ("5. Quit");
         System.out.println ("......................");
     }   
+    public void detailConfirmation(){
+        String MY_USERNAME = "anliz";
+        String MY_PASSWORD = "Admin123";
+
+        int inputRequest = 3;
+        
+        //Prompt 
+        while (inputRequest > 0){
+            System.out.print("Enter your username: ");
+            String username = scanner.nextLine();
+
+            System.out.print ("Enter your password: ");
+            String password = scanner.nextLine();
+
+            boolean isUsername = username.equals(MY_USERNAME);
+            boolean isPassword = password.equals(MY_PASSWORD);
+            if(isUsername && isPassword){
+                System.out.println("USER DETAILS CONFIRMED");
+                displayPage();
+                break;
+            }           
+            else {
+                System.err.println("Incorrect username or password.");
+                System.out.println(" ");
+                
+            }
+            inputRequest --;
+            System.out.println("Enter details again: ");   
+            System.out.println(+ inputRequest + " remaining entries left");
+            System.out.println("No more attempts."); 
+    }
     
+    //displayPage();
+
+    scanner.close(); 
+    } 
 }
--- src/main/java/com/systechafrica/AtmMachine/Attempt.java
+++ src/main/java/com/systechafrica/AtmMachine/Attempt.java
@@ -4,40 +4,59 @@ import java.util.Scanner;
 
 public class Attempt {
     public static void main(String[] args) {
-       
-         Scanner scanner = new Scanner(System.in);
-           // detailConfirmation();
-            String MY_USERNAME = "anliz";
-            String MY_PASSWORD = "Admin123";
+        
+        Scanner scanner = new Scanner(System.in);
 
-            int inputRequest = 3;
-            
+        double operatingBalance = 1000;
+        System.out.println("Choose service option:");
+        String option = scanner.nextLine();
 
-            //Prompt one
-            while (inputRequest > 1){
-                System.out.print("Enter your username: ");
-                String username = scanner.nextLine();
-
-                System.out.print ("Enter your password: ");
-                String password = scanner.nextLine();
-
-                boolean isUsername = username.equals(MY_USERNAME);
-                boolean isPassword = password.equals(MY_PASSWORD);
-                if(isUsername && isPassword){
-                    System.out.println("USER DETAILS CONFIRMED");
-                }           
-                else {
-                    System.err.println("Incorrect username or password.");
-                    System.out.println(" ");
-                    
-                }
-                System.out.println("Enter details again: ");   
-                System.out.println(+ inputRequest + " remaining entries left");
-                inputRequest --;
+        if(option.equals("1")){
+             System.out.println("Your Balance is Ksh." + operatingBalance);
+             //then
+             System.out.println("Select another service:");  
         }
-        System.out.println("Detail entry attempts exhausted.");  
-        scanner.close();
-        }  
-
-    
+        else if (option.equals("2")){
+            System.out.println ("Enter amount to deposit "); 
+            int depositAmount = scanner.nextInt();
+            operatingBalance = depositAmount + operatingBalance;
+            System.out.println("Your Balance is Ksh." + operatingBalance);
+        }
+        else if (option.equals("3")){
+            System.out.println ("Enter amount to withdraw ");
+            int withdrawAmount = scanner.nextInt();
+            if (operatingBalance > withdrawAmount){
+                double withdrwalCharges = 0.02 * withdrawAmount;
+                operatingBalance =  operatingBalance - withdrawAmount - (double) withdrwalCharges;
+                System.out.println("Your witdrawal charge is Ksh." + withdrwalCharges);
+                System.out.println("Your Balance is Ksh." + operatingBalance);
+                }
+                else {
+                    System.out.println("Go back to the main menu ");
+            }
+        }
+        else if (option.equals("4")){
+            System.out.println ("Enter amount to tranfer ");
+             int transferAmount = scanner.nextInt();
+            if (operatingBalance > transferAmount){
+                operatingBalance =  operatingBalance - transferAmount;
+                System.out.println("Your Balance is Ksh." + operatingBalance);
+                }
+                else {
+                    System.out.println("Go back to the main menu ");
+                }
+            }
+        else if (option.equals("5")){
+            System.exit(0);
+        }
+        else {
+            System.out.println("You entered an invalid option.");
+        }
+        scanner.close();    
+    }
 }
+
+
+
+    //app.inputUsernameAndPassword();
+       //app.operatingBalance();
