8f28b89affdf50e9b7894a8dfc2c3ec1bb52bc2d
Anliza <nnanliza.gmail.com>
2023-09-02 13:07:48 +0300

    working with string and stribuilder

---
+++ src/main/java/com/systechafrica/stringandstringbuilder/StringClassDemo.java
@@ -0,0 +1,24 @@
+package com.systechafrica.stringandstringbuilder;
+
+import java.util.logging.Logger;
+
+public class StringClassDemo {
+    private static final Logger LOGGER = Logger.getLogger(StringClassDemo.class.getName());
+
+    public static void main(String[] args) {
+        
+        //? strings are immutable i.e. once created they cannot be modified
+        //? unless they are reassigned
+    
+
+        //! Any operations on a string variable results in the creation of a new string => immutability
+        String message ="Hello";
+        //no effect
+        message.concat("world");
+        //has effect by reassignment 
+        message = message.concat(" programmer");
+        LOGGER.info(message);
+
+
+    }
+}
