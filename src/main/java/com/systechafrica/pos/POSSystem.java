899b4884d798ee8adf45f373b96fe6cb3f9644e1
Anliza <nnanliza.gmail.com>
2023-09-09 22:17:34 +0300

    pos all methds running

--- src/main/java/com/systechafrica/pos/POSSystem.java
+++ src/main/java/com/systechafrica/pos/POSSystem.java
@@ -12,6 +12,12 @@ public class POSSystem {
     int quantity;
     double unitPrice;
     double total;
+    Items buy = new Items(); 
+    boolean itemed = false;
+    int itemNumber = 1;
+    double sumOfTotalValue;
+    double amountByCustomer;
+    double changeBack;
     public static void main(String[] args) {
         POSSystem pos = new POSSystem();
         pos.addItem();
@@ -48,20 +54,65 @@ public class POSSystem {
     }
     
     public void addItem(){
-        System.out.print("Enter Item Code: ");
-        itemCode = scanner.nextInt();
-        System.out.print("Enter Quantity: ");
-        quantity = scanner.nextInt();
-        System.out.print("Enter the Unit Price: ");
-        unitPrice = scanner.nextDouble();
-        total = quantity * unitPrice;
-        System.out.println(total);
+        int noOfItems = 1;
+        while(noOfItems >= 1){
+            
+            System.out.println("item: " + itemNumber);
+
+            System.out.print("Enter Item Code: ");
+            buy.itemCode = scanner.nextInt();
+            System.out.print("Enter Quantity: ");
+            buy.quantity = scanner.nextInt();
+            System.out.print("Enter the Unit Price: ");
+            buy.unitPrice = scanner.nextDouble();
+            buy.total = buy.quantity*buy.unitPrice;
+            System.out.println("Total value: " + buy.total);
+            itemNumber ++;
+            System.out.print("More items? Press any key for more items or N for no more item:-> ");
+            String moreItems = scanner.next();
+          //scanner.nextLine();
+                if(moreItems.equalsIgnoreCase("N")){
+                    break;
+                }
+            }
+            noOfItems++;
     }
 
     public void makePayment(){
-        
-       // System.out.println("Item Code", "Quantity", " Unit Price", "Total Value");
-        //System.out.println( itemCode, quantity, unitPrice, total );
+        System.out.println("Ask payment from customer.");
+        amountByCustomer = scanner.nextDouble();
+        if (amountByCustomer > sumOfTotalValue){
+            changeBack = amountByCustomer - sumOfTotalValue;
+        }
+        else{
+            System.out.println("The amount is insufficient. Add some more. ");
+        }
     }
 
+    public void sumOfTotals(){
+        sumOfTotalValue = 0.0;
+        int i = 0;
+        while (i < itemNumber){
+            sumOfTotalValue += buy.total;
+            break;
+        }
+        i++;
+    }
+
+    public void displayReceipt(){
+        System.out.print("Item Code  "+ "Quantity  "+ "Unit Price   "+ "Total Value");
+        System.out.println();
+        for (int i = 1; i<=itemNumber; i++) {
+            Items buy = new Items(i, i, i, i);
+            System.out.println("   " + buy.itemCode+"         " + buy.quantity+ "          " + buy.unitPrice+ "          " + buy.total);
+        }
+        System.out.println("*********************************************");
+        System.out.println("Total: "  + sumOfTotalValue);
+        System.out.println("*********************************************");
+        System.out.println("Enter the amount given by customer:  " + amountByCustomer);
+        System.out.println("Change:  " + changeBack);
+        System.out.println("*********************************************");
+        System.out.println("THANK YOU FOR SHOPPING WITH US");
+        System.out.println("*********************************************");
+    }
 }
--- src/main/java/com/systechafrica/pos/Table.java
+++ src/main/java/com/systechafrica/pos/Table.java
@@ -8,17 +8,23 @@ public class Table {
     Items buy = new Items(); 
     boolean itemed = false;
     int itemNumber = 1;
+    double sumOfTotalValue;
+    double amountByCustomer;
+    double changeBack;
     Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Table table =new Table();
         table.items();
+        table.sumOfTotalValue();
+        table.change();
         table.listArray();
+        
+       
     
 }
 
     public void items(){
        int noOfItems = 1;
-       
         while(noOfItems >= 1){
             //Items buy = new Items(); //object is buy
             System.out.println("item: " + itemNumber);
@@ -46,15 +52,43 @@ public class Table {
     }
 
     public void listArray(){
-        System.out.print("Item Code  "+ "Quantity  "+ " Unit Price   "+ "Total Value");
+        System.out.print("Item Code  "+ "Quantity  "+ "Unit Price   "+ "Total Value");
         System.out.println();
         for (int i = 1; i<=itemNumber; i++) {
             Items buy = new Items(i, i, i, i);
             System.out.println("   " + buy.itemCode+"         " + buy.quantity+ "          " + buy.unitPrice+ "          " + buy.total);
         }
+        System.out.println("*********************************************");
+        System.out.println("Total: "  + sumOfTotalValue);
+        System.out.println("*********************************************");
+        System.out.println("Enter the amount given by customer:  " + amountByCustomer);
+        System.out.println("Change:  " + changeBack);
+        System.out.println("*********************************************");
+        System.out.println("THANK YOU FOR SHOPPING WITH US");
+        System.out.println("*********************************************");
+    }
+    
+    public void sumOfTotalValue(){
         
-        System.out.println("********************************");
-      
-
-}
+        sumOfTotalValue = 0.0;
+        int i = 0;
+        while (i < itemNumber){
+            sumOfTotalValue += buy.total;
+            break;
+        }
+        i++;
+       
+    }
+    
+    public void change(){
+        System.out.println("Ask payment from customer.");
+        amountByCustomer = scanner.nextDouble();
+        if (amountByCustomer > sumOfTotalValue){
+            changeBack = amountByCustomer - sumOfTotalValue;
+        }
+        else{
+            System.out.println("The amount is insufficient. Add some more. ");
+        }
+    }
+    
 }
