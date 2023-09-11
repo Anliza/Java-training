23a279775804323c3a5d742df26bcf795d4502c1
Anliza <nnanliza.gmail.com>
2023-09-12 02:05:45 +0300

    Array implemented on HotelMenu.java and it works

--- src/main/java/com/systechafrica/hotelmenu/HotelMenu.java
+++ src/main/java/com/systechafrica/hotelmenu/HotelMenu.java
@@ -7,6 +7,8 @@ public class HotelMenu {
     final String DEFAULT_PASSWORD = "Admin123";
     final int MAX_OPTIONS = 50;
     Options[] options = new Options[MAX_OPTIONS];
+    int noOfOptions = 0;
+    Options opt = new Options();
     static int mealTotal;
     static int payamentAmount;
     static int option;
@@ -15,11 +17,9 @@ public class HotelMenu {
 
     public static void main(String[] args) {
         HotelMenu menu = new HotelMenu();
-        Options opt = new Options();
         Scanner scan = new Scanner(System.in);
         
         //int noOfOptions = 1;
-        boolean optionsSelected = false;
         //boolean firstChoiceSelected = false;
 
         boolean logInSuccess = menu.loginH();
@@ -29,85 +29,8 @@ public class HotelMenu {
             boolean outputMenu = true;
             while (outputMenu){
                 menu.displayMenu();
-                System.out.print("Enter your meal/drink option: ");
-                option = scan.nextInt();
-                System.out.println(option);
-
-                switch (option){
-                    case 1:
-                        opt.setOption("Chai------------------------15");
-                        opt.setPrice(15);
-                        break;
-                    case 2:
-                        opt.setOption("Andazi----------------------10");
-                        opt.setPrice(10);
-                        break; 
-                    case 3:
-                        opt.setOption("Tosti-----------------------12");
-                        opt.setPrice(12);
-                        break; 
-                    case 4:
-                        opt.setOption("Ndengu and accomplishments--70");
-                        opt.setPrice(70);
-                        break;  
-                    case 5:
-                        opt.setOption("Beans and accomplishments----70");
-                        opt.setPrice(70);
-                        break;
-                    case 6:
-                        opt.setOption("Pilau Veg--------------------90");
-                        opt.setPrice(90);
-                        break;
-                    case 7:
-                        System.out.println("Thank You");
-                        break;
-                
-                }
-
-            // firstChoiceSelected =true;
-            // while(firstChoiceSelected){
-                    System.out.print("Do you want to enter another meal/drink option: ");
-                    String anotherOption = scan.next();
-                    if(anotherOption.equals("N")){
-                        optionsSelected = true;
-                        break;
-                    }else if(anotherOption.equals("Y")){
-                        //firstChoiceSelected = true;
-                        //noOfOptions++;
-                        
-                    }else{
-                        System.out.println();
-                        System.out.println("You have entered an invalid option. Enter Y/N for yes or No");
-                    }       
-            // }
-            //break;
-            }
-            System.out.println();
-            System.out.println("-------------------");
-            while (optionsSelected){
-                System.out.println("Proceed to Payment: ");
-                String toPayament = scan.next();
-                if(toPayament.equals("N")){
-                    System.out.println("choose Y for yes and N for no");
-                }
-                else if(toPayament.equals("Y")){
-                    System.out.println("Pay now for:");
-
-                    System.out.println(opt.getOption());
-                    //menu.listOptions();
-                    
-                    //get data for the option
-                    System.out.println("************************");
-                    System.out.println("Total:-------------------" + mealTotal);
-                    System.out.println("Enter amount to Pay:");
-                    payamentAmount = scan.nextInt();
-                    System.out.println("Your Balance is--------" + balanceBack());
-                    System.out.println("************************");
-                    break;
-                }
-                else{
-                    System.out.println("choose Y for yes and N for no");
-                }
+                menu.cashierScreen();
+                break;
             }
         }
         else{
@@ -155,18 +78,6 @@ public class HotelMenu {
         System.out.println("Choose your Drink/Meal-one at a time:");
     }
 
-    public int mealAmount(int a,int b,int ...otherNumbers){
-        Options opt = new Options();
-
-        a = opt.getPrice();
-        mealTotal = a+b;
-
-        for (int i=0; i < otherNumbers.length; i++){
-            mealTotal+= otherNumbers[i];
-        }
-        return mealTotal;
-
-        }
     
     public static int balanceBack(){
         int balance = 0;
@@ -178,55 +89,67 @@ public class HotelMenu {
     }
     
     public void optionss(){
-        Options opt = new Options();
-        switch (option){
-            case 1:
-                opt.setOption("Chai------------------------15");
-                break;
-            case 2:
-                opt.setOption("Andazi----------------------10");
-                break; 
-            case 3:
-                opt.setOption("Tosti-----------------------12");
-                break; 
-            case 4:
-                opt.setOption("Ndengu and accomplishments--70");
-                break;  
-            case 5:
-                opt.setOption("Beans and accomplishments----70");
-                break;
-            case 6:
-                opt.setOption("Pilau Veg--------------------12");
-                break;
-            case 7:
-                System.out.println("Thank You");
-                break;
-                
-        }
+       
     }
 
     public void cashierScreen(){
-        int noOfOptions = 1;
+        
         boolean optionsSelected = false;
         //boolean firstChoiceSelected = false;
-        while (noOfOptions > 0){
+        while (true){
             System.out.print("Enter your meal/drink option: ");
             option = scanner.nextInt();
             System.out.println(option);
-           // firstChoiceSelected =true;
-           // while(firstChoiceSelected){
-                System.out.print("Do you want to enter another meal/drink option: ");
-                String anotherOption = scanner.next();
-                if(anotherOption.equalsIgnoreCase("N")){
-                    optionsSelected = true;
-                    break;
-                }else if(anotherOption.equalsIgnoreCase("Y")){
-                    //firstChoiceSelected = true;
-                    noOfOptions++;
-                    
-                }else{
-                    System.out.println("You have entered an invalid option. Enter Y/N for yes or No");
+
+            switch (option){
+                    case 1:
+                        opt.setOption("Chai------------------------15");
+                        opt.setPrice(15);
+                        break;
+                    case 2:
+                        opt.setOption("Andazi----------------------10");
+                        opt.setPrice(10);
+                        break; 
+                    case 3:
+                        opt.setOption("Tosti-----------------------12");
+                        opt.setPrice(12);
+                        break; 
+                    case 4:
+                        opt.setOption("Ndengu and accomplishments--70");
+                        opt.setPrice(70);
+                        break;  
+                    case 5:
+                        opt.setOption("Beans and accomplishments----70");
+                        opt.setPrice(70);
+                        break;
+                    case 6:
+                        opt.setOption("Pilau Veg--------------------90");
+                        opt.setPrice(90);
+                        break;
+                    case 7:
+                        System.out.println("Thank You");
+                        break;
+                
                 }
+            
+            Options selOptions = new Options(opt.getOption(), opt.getPrice());
+            options[noOfOptions] = selOptions;
+            noOfOptions++;
+
+           
+           // while(firstChoiceSelected){
+            System.out.print("Do you want to enter another meal/drink option: ");
+            String anotherOption = scanner.next();
+            if(anotherOption.equalsIgnoreCase("N")){
+                optionsSelected = true;
+                break;
+            }else if(anotherOption.equalsIgnoreCase("Y")){
+                optionsSelected = true;
+                
+                
+            }else{
+                System.out.println("You have entered an invalid option. Enter Y/N for yes or No");
+            }
                 
            // }
         //break;
@@ -240,6 +163,13 @@ public class HotelMenu {
             }
             else if(toPayament.equalsIgnoreCase("Y")){
                 System.out.println("Pay now for:");
+                mealTotal = 0;
+                for(int i = 0; i < noOfOptions; i++){
+                    Options newOptions = options[i]; 
+                    System.out.println(newOptions.getOption());
+
+                    mealTotal += newOptions.getPrice();
+                }
                 //get data for the option
                 System.out.println("************************");
                 System.out.println("Total:-------------------" + mealTotal);
@@ -255,12 +185,5 @@ public class HotelMenu {
         }
     }
     
-    public void listOptions(){
-        
-        for (int i = 1; i < 50; i++){
-            Options opt = options[i];
-            System.out.println(options.length);
-            System.out.println("i am running");
-        }
-    }
+    
 }
