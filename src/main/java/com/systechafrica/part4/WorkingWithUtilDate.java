6f4d4f25eb25550663e2a28390dd9518cc3d600a
Anliza <nnanliza.gmail.com>
2023-10-03 06:46:53 +0300

    Working with date, new date and time API

---
+++ src/main/java/com/systechafrica/part4/WorkingWithUtilDate.java
@@ -0,0 +1,46 @@
+package com.systechafrica.part4;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class WorkingWithUtilDate {
+    public static void main(String[] args) {
+        Date date= new Date();
+        System.out.println(date);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 2);
+
+        //add two days to the date
+        System.out.println(calendar.getTime());
+
+        try (Scanner scanner =new Scanner(System.in)) {
+            Date todayDate = new Date();
+            System.out.println(todayDate);
+            
+            System.out.println("Enter date: ");
+            String dateString =scanner.nextLine();
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+             DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
+
+            Date tomorrowDate = df.parse(dateString);
+            System.out.println(tomorrowDate.getTime());
+            System.out.println(df2.format(tomorrowDate));
+
+            System.out.println(tomorrowDate.before(todayDate));
+            System.out.println(tomorrowDate.after(todayDate));
+            if (tomorrowDate.after(todayDate)) {
+                System.out.println("valid date");
+            }else{
+                System.out.println("please enter future date");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
